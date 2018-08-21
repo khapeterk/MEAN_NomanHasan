ea6331c39d345da4dadabeb2b8a008accba6411a
Peter Kha <khapeterk@gmail.com>
2018-08-21 00:27:44 -0700

    todo service

---
+++ todoapp/services/todo.service.js
@@ -0,0 +1,73 @@
+var ToDo = require('../models/todo.model')
+
+_this = this
+exports.getTodos = async function(query, page, limit) {
+  var options = {
+    page,
+    limit
+  }
+
+  try {
+    var todos = await ToDo.paginate(query, options)
+    return todos;
+  } catch(e) {
+    throw Error('Error while Paginating Todos')
+  }
+}
+
+exports.createTodo = async function(todo) {
+  var newTodo = new ToDo({
+    title: todo.title,
+    description: todo.description,
+    date: new Date(),
+    status: todo.status
+  })
+
+  try {
+    var savedTodo = await newTodo.save()
+    return savedTodo;
+  } catch(e) {
+    throw Error("Error while Creating Todo")
+  }
+}
+
+exports.updateTodo = async function(todo) {
+  var id = todo.id
+
+  try {
+    var toldTodo = await ToDo.findById(id)
+  } catch(e) {
+    throw Error("Error occured while finding the Todo")
+  }
+
+  if (!oldTodo) {
+    return false;
+  }
+
+  console.log(oldTodo)
+
+  oldTodo.title = todo.title
+  oldTodo.description = todo.description
+  oldTodo.status = todo.status
+
+  console.log(oldTodo)
+
+  try {
+    var savedTodo = await oldTodo.save()
+    return savedTodo
+  } catch(e) {
+    throw Error("And Error occured while updating the Todo")
+  }
+}
+
+exports.deleteTodo = async function(id) {
+  try {
+    var deleted = await ToDo.remove({_id: id})
+    if (deleted.result.n === 0) {
+      throw Error("Todo could not be deleted")
+    }
+    return deleted
+  } catch(e) {
+    throw Error("Error occured while deleting the Todo")
+  }
+}
