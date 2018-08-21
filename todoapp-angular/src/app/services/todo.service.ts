9fd56b114230e3fc1411af5f4f982cd340a96f0b
Peter Kha <khapeterk@gmail.com>
2018-08-21 09:56:46 -0700

    todo service methods

--- todoapp-angular/src/app/services/todo.service.ts
+++ todoapp-angular/src/app/services/todo.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { ToDo } from "../models/todo.model";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators"
 
 @Injectable()
 export class TodoService {
@@ -7,4 +10,37 @@ export class TodoService {
   todoUrl = `${this.api_url}/api/todos`;
 
   constructor(private http: HttpClient) {}
+
+  createTodo(todo: ToDo): Observable<any> {
+    return this.http.post(`${this.todoUrl}`, todo);
+  }
+
+  getToDos(): Observable<ToDo[]> {
+    return this.http.get(this.todoUrl)
+      .pipe(
+        map(res => {
+          return res["data"].docs as ToDo[];
+        })
+      )
+  }
+
+  editTodo(todo: ToDo) {
+    let editUrl = `${this.todoUrl}`;
+    return this.http.put(editUrl, todo);
+  }
+
+  deleteTodo(id: string): any {
+    let deleteUrl = `${this.todoUrl}/${id}`;
+    return this.http.delete(deleteUrl)
+      .pipe(
+        map(res => {
+          return res;
+        })
+      )
+  }
+
+  private handError(error: any): Promise<any> {
+    console.error("An error occurred", error);
+    return Promise.reject(error.message || error);
+  }
 }
