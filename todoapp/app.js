bf447f792b7d666cf696cfa8e4733fe1f2c7758a
Peter Kha <khapeterk@gmail.com>
2018-08-21 00:09:06 -0700

    CORS configuration

--- todoapp/app.js
+++ todoapp/app.js
@@ -45,4 +45,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/todoapp', {useMongoClient: true})
   .then(()=> { console.log (`Successfully connected to the Mongodb Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
   .catch(()=> { console.log(`Error connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+})
+
 module.exports = app;
